34400937f61bcd723cf540b77bd7ecf4701a91c8
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-04-08 00:02:12 +0000

    import Header and Footer

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,49 +1,19 @@
-import React, { useContext, useState } from 'react';
-
-import { Link, Outlet, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../context/LogContext'
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer'
 
 function RouterLayout() {
-    const navigate = useNavigate();
-    const { isLogged, setLogged } = useContext(AuthContext);
-
-
     return (
-        <div className="container">
-            <header>
-                <nav className="navbar">
-                    <ul className="navbar-list">
-                        <li className="navbar-item">
-                            <Link to="/" className="navbar-link">Home</Link>
-                        </li>
-
-                        {!isLogged && <>
-                            <li className="navbar-item">
-                                <Link to="/register" className="navbar-link">Register</Link>
-                            </li>
-                            <li className="navbar-item">
-                                <Link to="/login" className="navbar-link">Login</Link>
-                            </li>
-                        </>}
-
-                        {isLogged && <>
-                            <li className="navbar-item">
-                                <Link to="/profile" className="navbar-link">Profile</Link>
-                            </li>
-                            <li className="navbar-item">
-                                <Link to="/posts" className="navbar-link">Posts</Link>
-                            </li>
-                            <li className="navbar-item">
-                                <Link to="/logout" className="navbar-link">Logout</Link>
-                            </li>
-                        </>}
-                    </ul>
-                </nav>
-            </header>
-            <div className="content">
+        <div className="site-wrapper">
+            <Header />
+            <main>
                 <Outlet />
-            </div>
+            </main>
+            <Footer />
+
         </div>
-    );
+    )
 }
+
 export default RouterLayout
