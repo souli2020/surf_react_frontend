7166f17600422040a72d56cb02f87d7f2c0fcba6
mimoune soulimani <soulimaniprosoulim@gmail.com>
2023-04-12 23:19:55 +0000

    Add refresh state

--- src/context/LogContext.jsx
+++ src/context/LogContext.jsx
@@ -4,11 +4,12 @@ const AuthContext = createContext();
 
 function AuthContextProvider(props) {
     const [isLogged, setLogged] = useState(false);
+    const [refresh, setRefresh] = useState(false);
     const token = localStorage.getItem('token');
 
 
     return (
-        <AuthContext.Provider value={{ isLogged, setLogged, token }}>
+        <AuthContext.Provider value={{ isLogged, setLogged, setRefresh, refresh, token }}>
             {props.children}
         </AuthContext.Provider>
     );
